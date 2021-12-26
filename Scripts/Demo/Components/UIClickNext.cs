6ced791e79427001d1a67f10c58f3b61c22e4851
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2021-12-26 11:14:13 +0100

    fix problem with loading, insert support for no existing blend, insert test scenes

---
+++ Scripts/Demo/Components/UIClickNext.cs
@@ -0,0 +1,13 @@
+using UnityEngine.EventSystems;
+using UnitySceneEx.Runtime.scene_system.scene_ex.Scripts.Runtime.Components;
+
+namespace UnitySceneEx.Demo.scene_system.scene_ex.Scripts.Demo.Components
+{
+    public sealed class UIClickNext : UIBehaviour
+    {
+        public void HandleNext()
+        {
+            SceneSystem.Singleton.Load("next");
+        }
+    }
+}
