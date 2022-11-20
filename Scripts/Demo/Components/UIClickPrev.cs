c63b0d1918ec607dd09477d051f4aab7ccf391e2
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-11-20 20:24:46 +0100

    fix scene parameter problem

--- Scripts/Demo/Components/UIClickPrev.cs
+++ Scripts/Demo/Components/UIClickPrev.cs
@@ -1,13 +1,27 @@
+#if DEMO
+using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnitySceneEx.Demo.scene_system.scene_ex.Scripts.Demo.Parameters;
+using UnitySceneEx.Runtime.scene_system.scene_ex.Scripts.Runtime;
 using UnitySceneEx.Runtime.scene_system.scene_ex.Scripts.Runtime.Components;
 
 namespace UnitySceneEx.Demo.scene_system.scene_ex.Scripts.Demo.Components
 {
     public sealed class UIClickPrev : UIBehaviour
     {
+        [SerializeField]
+        private TextMeshProUGUI text;
+
+        protected override void Start()
+        {
+            text.text = SceneParameter.Get<DemoNextParameter>().Text;
+        }
+        
         public void HandleClickPrev()
         {
-            SceneController.Singleton.Load("prev");
+            SceneController.Singleton.Load("prev", new DemoPrevParameter {Text = "Hello"});
         }
     }
 }
+#endif
---
+++ Scripts/Demo/Parameters/DemoPrevParameter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnitySceneBase.Runtime.scene_system.scene_base.Scripts.Runtime.Types;
+
+namespace UnitySceneEx.Demo.scene_system.scene_ex.Scripts.Demo.Parameters
+{
+    public interface IDemoPrevParameter
+    {
+        public string Text { get; }
+    }
+    
+    [ParameterInitialDataType(typeof(DemoPrevParameterInitialData))]
+    public sealed class DemoPrevParameter : ParameterData, IDemoPrevParameter
+    {
+        public string Text
+        {
+            get => Get<string>("text");
+            set => Add("text", value);
+        }
+
+        public override void InitializeData(ScriptableObject initData)
+        {
+            var data = (DemoPrevParameterInitialData)initData;
+            if (data == null)
+                return;
+
+            Text = data.Text;
+        }
+    }
+
+    public sealed class DemoPrevParameterInitialData : ScriptableObject, IDemoPrevParameter
+    {
+        [SerializeField]
+        private string text;
+
+        public string Text => text;
+    }
+}
